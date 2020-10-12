69748779891b5ec52530fc26010af3d9ffc55f9d
Grzegorz Ilnicki PC <ggilnicki@gmail.com>
2020-10-12 10:54:51 +0200

    Initial project setup

---
+++ upjs-net/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace upjs
+{
+    public static class upjs
+    {
+    }
+
+    
+}
